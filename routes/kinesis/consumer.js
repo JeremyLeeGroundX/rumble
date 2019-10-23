f96454d0b7347719871db3f4a8b1b50759bae866
jeremy.lee <jeremy.lee@groundx.xyz>
2019-10-23 13:56:22 +0900

    logger

--- routes/kinesis/consumer.js
+++ routes/kinesis/consumer.js
@@ -1,5 +1,5 @@
 var AWS = require('aws-sdk');
-var log = require('../logger');
+var logger = require('../logger');
 var util = require('util');
 var config = require('../../config');
 var kinesis = new AWS.Kinesis({region : config.kinesis.region,
@@ -15,36 +15,6 @@ var options = {
 };
 
 
-// read = async function() {
-//   let streamRes = await kinesis.describeStream({StreamName: config.kinesis.stream}).promise();
-//
-//   console.log("length:", streamRes.StreamDescription.Shards.length);
-//
-//   // now get the Stream's shardIterator and start getting records in a loop
-//   let shardIt = await kinesis.getShardIterator({
-//     StreamName:config.kinesis.stream,
-//     ShardId:data.StreamDescription.Shards[0].ShardId,
-//     ShardIteratorType:'LATEST'
-//   }).promise();
-//
-//   console.log("shardIt:", shardIt);
-//
-//   for(let it of shiardIt) {
-//     console.log('it:', it);
-//
-//     let data = await kinesis.getRecords({ShardIterator:it}).promise();
-//     console.log("rec:", rec);
-//
-//     for(let record of data.Records) {
-//       console.log(data.Records[record].Data);
-//
-//     }
-//
-//   }
-//
-//
-//   return streamRes;
-// }
 var readable;
 startConsume = function() {
   readable = require('kinesis-readable')(kinesis, options);
@@ -56,17 +26,17 @@ startConsume = function() {
       let data = new Buffer(record.Data, 'base64').toString();
       sequenceNumber = record.SequenceNumber;
       partitionKey = record.PartitionKey;
-      log.info(util.format('Record: %s, SeqenceNumber: %s, PartitionKey:%s', data, sequenceNumber, partitionKey));
+      logger.info(util.format('Record: %s, SeqenceNumber: %s, PartitionKey:%s', data, sequenceNumber, partitionKey));
     }
   })
     .on('checkpoint', function(sequenceNumber) {
-      console.log('CHECKPOINT:', sequenceNumber);
+      logger.info('CHECKPOINT:', sequenceNumber);
     })
     .on('error', function(err) {
-      console.error(err);
+      logger.error(err);
     })
     .on('end', function() {
-      console.log('consume closed');
+      logger.info('consume closed');
     });
   logger.info("CONSUME STARTED");
   return "Start Consume";
